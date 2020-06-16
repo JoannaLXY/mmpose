a5935da2f7922485ab23f2a885f22688225c1c17
lizz <lizz@sensetime.com>
2020-06-16 17:47:56 +0800

    clean
    Signed-off-by: lizz <lizz@sensetime.com>

--- mmpose/core/post_processing/shared_transforms.py
+++ mmpose/core/post_processing/shared_transforms.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 
-def affine_transform(pt, trans):
+def affine_transform(pt, trans_mat):
     """Apply an affine transformation to the points.
 
     Args:
-        pt (np.ndarray): 2 dim point to be transformed
-        trans (np.ndarray): 2x3 matrix of an affine transform
+        pt (np.ndarray): a 2 dimensional point to be transformed
+        trans_mat (np.ndarray): 2x3 matrix of an affine transform
 
     Returns:
-        Transformed points.
+        Transformed points (np.ndarray)
     """
-    new_pt = np.matrix(trans) * np.matrix([pt[0], pt[1], 1.]).T
-    new_pt = np.array(new_pt)[:, 0]
+    new_pt = np.array(trans_mat) @ np.array([pt[0], pt[1], 1.])
+
     return new_pt
 
 
@@ -27,28 +27,28 @@ def get_3rd_point(a, b):
         b (np.ndarray): point(x,y)
 
     Returns:
-        The 3rd point is defined by rotate point a 90 degrees anticlockwise,
-    using b as rotation center.
+        The 3rd point (np.ndarray)
     """
     direct = a - b
-    return b + np.array([-direct[1], direct[0]], dtype=np.float32)
+    third_pt = b + np.array([-direct[1], direct[0]], dtype=np.float32)
+
+    return third_pt
 
 
-def get_dir(src_point, rot_rad):
-    """Rotate the vector by an angle
+def rotate_point(pt, angle_rad):
+    """Rotate a point by an angle
 
     Args:
-        src_point (list[float]): source point
-        rot_rad (float): rotation angle by radius
+        pt (list[float]): 2 dimensional point point to be rotated
+        angle_rad (float): rotation angle by radius
 
     Returns:
-        Rotation matrix
+        Rotated point (list[float])
     """
+    assert len(pt) == 2
+    sn, cs = np.sin(angle_rad), np.cos(angle_rad)
+    new_x = pt[0] * cs - pt[1] * sn
+    new_y = pt[0] * sn + pt[1] * cs
+    rotated_pt = [new_x, new_y]
 
-    sn, cs = np.sin(rot_rad), np.cos(rot_rad)
-
-    src_result = [0, 0]
-    src_result[0] = src_point[0] * cs - src_point[1] * sn
-    src_result[1] = src_point[0] * sn + src_point[1] * cs
-
-    return src_result
+    return rotated_pt
--- tests/post_processing/test_shared_transforms.py
+++ tests/post_processing/test_shared_transforms.py
@@ -3,25 +3,28 @@ from numpy.testing import assert_array_almost_equal
 
 from mmpose.core.post_processing.shared_transforms import (affine_transform,
                                                            get_3rd_point,
-                                                           get_dir)
+                                                           rotate_point)
 
 
 def test_get_3rd_point():
     a = np.array([0, 1])
     b = np.array([0, 0])
-    assert_array_almost_equal(
-        get_3rd_point(a, b), np.array([-1, 0]), decimal=4)
+    ans = get_3rd_point(a, b)
+    assert_array_almost_equal(ans, np.array([-1, 0]), decimal=4)
+    assert isinstance(ans, np.ndarray)
 
 
 def test_affine_transform():
     pt = np.array([0, 1])
     trans = np.array([[1, 0, 1], [0, 1, 0]])
-    assert_array_almost_equal(
-        affine_transform(pt, trans), np.array([1, 1]), decimal=4)
+    ans = affine_transform(pt, trans)
+    assert_array_almost_equal(ans, np.array([1, 1]), decimal=4)
+    assert isinstance(ans, np.ndarray)
 
 
-def test_get_dir():
+def test_rotate_point():
     src_point = np.array([0, 1])
-    rot_rad = np.pi / 2
-    assert_array_almost_equal(
-        get_dir(src_point, rot_rad), np.array([-1, 0]), decimal=4)
+    rot_rad = np.pi / 2.
+    ans = rotate_point(src_point, rot_rad)
+    assert_array_almost_equal(ans, np.array([-1, 0]), decimal=4)
+    assert isinstance(ans, list)
