96594335886a4c768bf92f20366c57e29119ecc9
wangcan <wangcan@sensetime.com>
2020-06-17 14:26:02 +0800

    rebase post_processing

--- mmpose/core/post_processing/__init__.py
+++ mmpose/core/post_processing/__init__.py
@@ -3,11 +3,6 @@ from .top_down_transforms import (flip_back, fliplr_joints,
                                   get_affine_transform, transform_preds)
 
 __all__ = [
-    'transform_preds',
-    'fliplr_joints',
-    'flip_back',
-    'get_affine_transform',
-    'affine_transform',
-    'rotate_point',
-    'get_3rd_point',
+    'get_3rd_point', 'affine_transform', 'rotate_point', 'flip_back',
+    'fliplr_joints', 'transform_preds', 'get_affine_transform'
 ]
