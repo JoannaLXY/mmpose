c76e3c3fa736917acbb960e69869ed5da00905bb
wangcan <wangcan@sensetime.com>
2020-06-17 15:33:26 +0800

    add nms and dataset and unit testing

--- mmpose/datasets/datasets/top_down/topdown_base_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_base_dataset.py
@@ -10,18 +10,18 @@ from mmpose.datasets.pipelines import Compose
 class TopDownBaseDataset(Dataset, metaclass=ABCMeta):
     """Base class for top-down datasets.
 
-        All top-down datasets should subclass it.
-        All subclasses should overwrite:
-            Methods:`_get_db`
+    All top-down datasets should subclass it.
+    All subclasses should overwrite:
+        Methods:`_get_db`
 
-        Args:
-            ann_file (str): Path to the annotation file.
-            img_prefix (str): Path to a directory where images are held.
-                Default: None.
-            data_cfg (dict): config
-            pipeline (list[dict | callable]): A sequence of data transforms.
-            test_mode (bool): Store True when building test or
-                validation dataset. Default: False.
+    Args:
+        ann_file (str): Path to the annotation file.
+        img_prefix (str): Path to a directory where images are held.
+            Default: None.
+        data_cfg (dict): config
+        pipeline (list[dict | callable]): A sequence of data transforms.
+        test_mode (bool): Store True when building test or
+            validation dataset. Default: False.
     """
 
     def __init__(self,
@@ -31,7 +31,6 @@ class TopDownBaseDataset(Dataset, metaclass=ABCMeta):
                  pipeline,
                  test_mode=False):
 
-        # TODO: set group flag for the sampler
         self.image_info = {}
         self.ann_info = {}
 
@@ -39,20 +38,20 @@ class TopDownBaseDataset(Dataset, metaclass=ABCMeta):
         self.img_prefix = img_prefix
         self.pipeline = pipeline
         self.test_mode = test_mode
-        self.use_gt_bbox = data_cfg.use_gt_bbox
-        self.bbox_file = data_cfg.bbox_file
-        self.image_thre = data_cfg.image_thre
 
-        self.ann_info['image_size'] = np.array(data_cfg.image_size)
-        self.ann_info['heatmap_size'] = np.array(data_cfg.heatmap_size)
-        self.ann_info['num_joints'] = data_cfg.num_joints
+        self.use_gt_bbox = data_cfg['use_gt_bbox']
+        self.bbox_file = data_cfg['bbox_file']
+        self.image_thre = data_cfg['image_thre']
+
+        self.ann_info['image_size'] = np.array(data_cfg['image_size'])
+        self.ann_info['heatmap_size'] = np.array(data_cfg['heatmap_size'])
+        self.ann_info['num_joints'] = data_cfg['num_joints']
 
         self.ann_info['flip_pairs'] = None
 
-        self.ann_info['model_select_channel'] = data_cfg.model_select_channel
-        self.ann_info['num_output_channel'] = data_cfg.num_output_channel
-        self.ann_info[
-            'model_supervise_channel'] = data_cfg.model_supervise_channel
+        self.ann_info['inference_channel'] = data_cfg['inference_channel']
+        self.ann_info['num_output_channels'] = data_cfg['num_output_channels']
+        self.ann_info['dataset_channel'] = data_cfg['dataset_channel']
 
         self.db = []
         self.pipeline = Compose(self.pipeline)
@@ -68,18 +67,7 @@ class TopDownBaseDataset(Dataset, metaclass=ABCMeta):
     def __len__(self, ):
         return len(self.db)
 
-    def prepare_train_img(self, idx):
-        results = copy.deepcopy(self.db[idx])
-        results['ann_info'] = self.ann_info
-        return self.pipeline(results)
-
-    def prepare_test_img(self, idx):
-        results = copy.deepcopy(self.db[idx])
-        results['ann_info'] = self.ann_info
-        return self.pipeline(results)
-
     def __getitem__(self, idx):
-        if self.test_mode:
-            return self.prepare_test_img(idx)
-        else:
-            return self.prepare_train_img(idx)
+        results = copy.deepcopy(self.db[idx])
+        results['ann_info'] = self.ann_info
+        return self.pipeline(results)
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,21 +2,49 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from mmpose.datasets.builder import DATASETS
+from mmpose.datasets import DATASETS
 
 
 @pytest.mark.parametrize('dataset', ['TopDownCocoDataset'])
 def test_custom_classes_override_default(dataset):
+    # test datasets
     dataset_class = DATASETS.get(dataset)
     dataset_class.load_annotations = MagicMock()
     if dataset in ['TopDownCocoDataset']:
         dataset_class.coco = MagicMock()
 
+    channel_cfg = dict(
+        num_joints=17,
+        sub_data_name=['coco'],
+        dataset_channel=[
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        ],
+        inference_channel=[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+        ])
+
+    data_cfg = dict(
+        image_size=[192, 256],
+        heatmap_size=[48, 64],
+        num_output_channels=channel_cfg['num_joints'],
+        num_joints=channel_cfg['num_joints'],
+        dataset_channel=channel_cfg['dataset_channel'],
+        inference_channel=channel_cfg['inference_channel'],
+        soft_nms=False,
+        nms_thre=1.0,
+        oks_thre=0.9,
+        in_vis_thre=0.2,
+        bbox_thre=1.0,
+        use_gt_bbox=True,
+        image_thre=0.0,
+        bbox_file='',
+    )
+
     # Test setting classes as a tuple
     custom_dataset = dataset_class(
-        ann_file=MagicMock(),
+        ann_file='tests/data/test.json',
         img_prefix='',
-        data_cfg=None,
+        data_cfg=data_cfg,
         pipeline=[],
         test_mode=True)
 
