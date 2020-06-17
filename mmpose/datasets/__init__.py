77e4190316d06bb6c3f7c973f3edb3e623aed39d
wangcan <wangcan@sensetime.com>
2020-06-17 17:35:49 +0800

    unit testing

--- mmpose/datasets/__init__.py
+++ mmpose/datasets/__init__.py
@@ -1,9 +1,9 @@
-from .builder import build_dataloader, build_dataset
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .datasets import TopDownCocoDataset
 from .pipelines import Compose
 from .samplers import DistributedSampler
 
 __all__ = [
     'TopDownCocoDataset', 'build_dataloader', 'build_dataset', 'Compose',
-    'DistributedSampler'
+    'DistributedSampler', 'DATASETS', 'PIPELINES'
 ]
