2b5c6d6de7c51810eeef5da0440eb3e83a85af5b
jinsheng <jinsheng@sensetime.com>
2020-06-19 18:00:41 +0800

    POSENET -> POSENETS

--- mmpose/models/__init__.py
+++ mmpose/models/__init__.py
@@ -1,8 +1,8 @@
 from .backbones import *  # noqa
-from .builder import (BACKBONES, HEADS, LOSSES, POSENET, build_backbone,
+from .builder import (BACKBONES, HEADS, LOSSES, POSENETS, build_backbone,
                       build_head, build_loss, build_posenet)
 
 __all__ = [
-    'BACKBONES', 'HEADS', 'LOSSES', 'POSENET', 'build_backbone', 'build_head',
+    'BACKBONES', 'HEADS', 'LOSSES', 'POSENETS', 'build_backbone', 'build_head',
     'build_loss', 'build_posenet'
 ]
