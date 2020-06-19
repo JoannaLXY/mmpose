7a675b83c6446edabbbb1920ce7f404dd11c5f7a
jinsheng <jinsheng@sensetime.com>
2020-06-19 22:35:32 +0800

    update tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,15 +1,10 @@
 import pytest
 import torch
+from mmpose.models.losses import JointsMSELoss, JointsOHKMMSELoss
 
 
 def test_mse_loss():
-    from mmpose.models import build_loss
-    from mmpose.models.losses import JointsMSELoss, JointsOHKMMSELoss
-
-    # test MSE loss without target weight
-    loss_cfg = dict(type='JointsMSELoss')
-    loss = build_loss(loss_cfg)
-
+    loss = JointsMSELoss()
     fake_pred = torch.zeros((1, 3, 64, 64))
     fake_label = torch.zeros((1, 3, 64, 64))
     assert torch.allclose(loss(fake_pred, fake_label, None), torch.tensor(0.))
@@ -24,29 +19,24 @@ def test_mse_loss():
     assert torch.allclose(loss(fake_pred, fake_label, None), torch.tensor(0.5))
 
     with pytest.raises(ValueError):
-        loss_cfg = dict(type='JointsOHKMMSELoss')
-        loss = build_loss(loss_cfg)
+        loss = JointsOHKMMSELoss()
         fake_pred = torch.zeros((1, 3, 64, 64))
         fake_label = torch.zeros((1, 3, 64, 64))
         assert torch.allclose(
             loss(fake_pred, fake_label, None), torch.tensor(0.))
 
     with pytest.raises(AssertionError):
-        loss_cfg = dict(type='JointsOHKMMSELoss', topk=-1)
-        loss = build_loss(loss_cfg)
+        loss = JointsOHKMMSELoss(topk=-1)
         fake_pred = torch.zeros((1, 3, 64, 64))
         fake_label = torch.zeros((1, 3, 64, 64))
         assert torch.allclose(
             loss(fake_pred, fake_label, None), torch.tensor(0.))
 
-    loss_cfg = dict(type='JointsOHKMMSELoss', topk=2)
-    loss = build_loss(loss_cfg)
+    loss = JointsOHKMMSELoss(topk=2)
     fake_pred = torch.ones((1, 3, 64, 64))
     fake_label = torch.zeros((1, 3, 64, 64))
     assert torch.allclose(loss(fake_pred, fake_label, None), torch.tensor(1.))
 
-    loss_cfg = dict(type='JointsOHKMMSELoss', topk=2)
-    loss = build_loss(loss_cfg)
     fake_pred = torch.zeros((1, 3, 64, 64))
     fake_pred[0, 0] += 1
     fake_label = torch.zeros((1, 3, 64, 64))
