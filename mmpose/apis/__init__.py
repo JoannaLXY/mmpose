07366b0dd602690288d480cf1d20a57c8fa922c3
wangcan <wangcan@sensetime.com>
2020-07-07 17:31:04 +0800

    patch_1_fix_bug

--- mmpose/apis/__init__.py
+++ mmpose/apis/__init__.py
@@ -1,10 +1,9 @@
 from .inference import (inference_pose_model, init_pose_model, save_pose_vis,
                         show_pose_result)
 from .test import multi_gpu_test, single_gpu_test
-from .train import set_random_seed, train_model
+from .train import train_model
 
 __all__ = [
-    'train_model', 'set_random_seed', 'init_pose_model',
-    'inference_pose_model', 'multi_gpu_test', 'single_gpu_test',
-    'show_pose_result', 'save_pose_vis'
+    'train_model', 'init_pose_model', 'inference_pose_model', 'multi_gpu_test',
+    'single_gpu_test', 'show_pose_result', 'save_pose_vis'
 ]
--- mmpose/core/optimizer/__init__.py
+++
@@ -1,3 +0,0 @@
-from .copy_of_sgd import CopyOfSGD
-
-__all__ = ['CopyOfSGD']
